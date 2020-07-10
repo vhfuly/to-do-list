1bb815c7845753674b79d2fe668c3959858a69e4
Victor Fuly <victor_hugo_fuly@hotmail.com>
2020-07-10 20:35:23 -0300

    EJS e configuração express

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require('express');
-const checklistRouter = require('./scr/routers/checklist');
-require('./config/database')
+const path = require('path');
 
-const app =  express();
+const checkListRouter = require('./src/routes/checklist');
+const rootRouter = require('./src/routes/index');
 
+
+require('./config/database');
+
+
+const app = express();
 app.use(express.json());
 
-app.use('/checklists',checklistRouter);
 
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
 
+app.use('/', rootRouter);
+app.use('/checklists', checkListRouter);
 
-app.listen(3000, ()=>{
-    console.log('Servidor foi iniciado')
+app.listen(3000, () => {
+  console.log('Servidor foi iniciado');
 })
